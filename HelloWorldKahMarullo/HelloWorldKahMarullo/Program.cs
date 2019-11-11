5cb9034bc13a551c576d7f6328dbfc13de18de7a
Arthur Kah <56394497+mrkah@users.noreply.github.com>
2019-11-11 16:02:22 +0100

    First Upload

---
+++ HelloWorldKahMarullo/HelloWorldKahMarullo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldKahMarullo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Kah Marullo, premier depot sur GitHub ^^");
+        }
+    }
+}
